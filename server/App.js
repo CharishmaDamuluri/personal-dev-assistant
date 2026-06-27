5b0e6370243f1b8fbcccf6b04d223e8ee7842a44
Charishma Damuluri <57269881+CharishmaDamuluri@users.noreply.github.com>
2026-06-27 10:51:53 -0500

    feat: Add UserInfoForm import and render it in App component

---
+++ server/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserInfoForm from './UserInfoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Main Application</h1>
+      <UserInfoForm />
+    </div>
+  );
+}
+
+export default App;
